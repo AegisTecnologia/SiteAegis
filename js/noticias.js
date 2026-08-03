a6262199635ff519f3c0a74724881837ae4e2a9a
Isabella <isabella07.silva@catolicasc.edu.br>
2026-08-03 15:03:20 -0300

    commit

--- js/noticias.js
+++ js/noticias.js
@@ -1,6 +1,56 @@
-const API_KEY = "AIzaSyCIv2bmzMyLw2GzbkJJ52rN70NLig8fFvo";
+const API_KEY = "AIzaSyBIAx2hqE0y0OGLVmgg3mFVyh-4wMD25Gw";
 const PASTA_ID = "1IA8ywA43Zw5rXWABSpAjKc8f-WEcmWmZ";
+const arquivoJSON = "noticias.json";
 
+
+fetch(arquivoJSON)
+
+.then(res => res.json())
+
+.then(noticias => {
+
+const area = document.getElementById("cards-noticias");
+
+
+noticias.forEach(noticia => {
+
+
+area.innerHTML += `
+
+<div class="card-noticia">
+
+<img src="${noticia.imagem}">
+
+
+<div class="conteudo-card">
+
+<h3>
+${noticia.titulo}
+</h3>
+
+
+<p>
+${noticia.texto}
+</p>
+
+
+<span>
+${noticia.data}
+</span>
+
+
+</div>
+
+</div>
+
+
+`;
+
+
+});
+
+
+});
 fetch(
 `https://www.googleapis.com/drive/v3/files?q='${PASTA_ID}'+in+parents&key=${API_KEY}&fields=files(name,id,webViewLink)`
 )
@@ -41,4 +91,49 @@ fetch(
 
 .catch(erro => {
     console.log("Erro ao carregar Drive:", erro);
+});
+
+fetch("arquivo-do-drive")
+.then(res => res.json())
+.then(noticias => {
+
+const area =
+document.getElementById("listaNoticias");
+
+
+noticias.forEach(noticia => {
+
+
+area.innerHTML += `
+
+<div class="card-noticia">
+
+
+<img src="${noticia.imagem}">
+
+
+<h3>
+${noticia.titulo}
+</h3>
+
+
+<p>
+${noticia.texto}
+</p>
+
+
+<span>
+${noticia.data}
+</span>
+
+
+</div>
+
+
+`;
+
+
+});
+
+
 });
