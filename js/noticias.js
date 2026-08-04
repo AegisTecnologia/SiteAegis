85b502e0ae9f7e4fedca30993e1ff6e83addd785
Isabella <isabella07.silva@catolicasc.edu.br>
2026-08-04 14:32:06 -0300

    commmit

--- js/noticias.js
+++ js/noticias.js
@@ -1,139 +1,44 @@
-const API_KEY = "AIzaSyBIAx2hqE0y0OGLVmgg3mFVyh-4wMD25Gw";
-const PASTA_ID = "1IA8ywA43Zw5rXWABSpAjKc8f-WEcmWmZ";
-const arquivoJSON = "noticias.json";
+const listaNoticias = document.getElementById("listaNoticias");
 
 
-fetch(arquivoJSON)
-
-.then(res => res.json())
-
+fetch("../noticias.json")
+.then(resposta => resposta.json())
 .then(noticias => {
 
-const area = document.getElementById("cards-noticias");
+
+    noticias.forEach(noticia => {
 
 
-noticias.forEach(noticia => {
-
-
-area.innerHTML += `
-
-<div class="card-noticia">
-
-<img src="${noticia.imagem}">
-
-
-<div class="conteudo-card">
-
-<h3>
-${noticia.titulo}
-</h3>
-
-
-<p>
-${noticia.texto}
-</p>
-
-
-<span>
-${noticia.data}
-</span>
-
-
-</div>
-
-</div>
-
-
-`;
-
-
-});
-
-
-});
-fetch(
-`https://www.googleapis.com/drive/v3/files?q='${PASTA_ID}'+in+parents&key=${API_KEY}&fields=files(name,id,webViewLink)`
-)
-
-.then(res => res.json())
-
-.then(data => {
-
-    console.log(data); // ajuda a descobrir erros
-
-    let area = document.getElementById("lista-drive");
-
-    if (!data.files) {
-        area.innerHTML = "Nenhum arquivo encontrado.";
-        return;
-    }
-
-
-    data.files.forEach(arquivo => {
-
-        area.innerHTML += `
+        listaNoticias.innerHTML += `
 
         <div class="card">
 
-            <h3>${arquivo.name}</h3>
+            <h2>
+                ${noticia.titulo}
+            </h2>
 
-            <a href="${arquivo.webViewLink}" target="_blank">
-                Abrir material
-            </a>
+            <p>
+                ${noticia.conteudo}
+            </p>
+
+            <span>
+                ${new Date(noticia.data).toLocaleDateString("pt-BR")}
+            </span>
 
         </div>
 
         `;
 
+
     });
 
+
 })
-
 .catch(erro => {
-    console.log("Erro ao carregar Drive:", erro);
-});
 
-fetch("arquivo-do-drive")
-.then(res => res.json())
-.then(noticias => {
-
-const area =
-document.getElementById("listaNoticias");
-
-
-noticias.forEach(noticia => {
-
-
-area.innerHTML += `
-
-<div class="card-noticia">
-
-
-<img src="${noticia.imagem}">
-
-
-<h3>
-${noticia.titulo}
-</h3>
-
-
-<p>
-${noticia.texto}
-</p>
-
-
-<span>
-${noticia.data}
-</span>
-
-
-</div>
-
-
-`;
-
-
-});
-
+    console.error(
+        "Erro carregando notícias:",
+        erro
+    );
 
 });
